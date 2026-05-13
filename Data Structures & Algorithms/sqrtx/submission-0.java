33fa5b485f0a2242646e7c9798eb3072879417bf
Prince kumar <121077708+rohanprince@users.noreply.github.com>
2026-05-13 09:54:41 +0530

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x==0){
+            return 0;
+        }
+        int ans = 1;
+        for(int i=1;i<=x;i++){
+            if((long) i * i > x){
+                return ans;
+            }
+            ans = i;
+        }
+        return ans;
+    }
+}
