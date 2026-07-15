48adf86c3df43e41af917f12a6a04d6406fd2de9
Prince kumar <121077708+rohanprince@users.noreply.github.com>
2026-07-15 09:23:49 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> perms = new ArrayList<>();
+        perms.add(new ArrayList<>());
+
+        for (int num : nums) {
+            List<List<Integer>> new_perms = new ArrayList<>();
+            for (List<Integer> p : perms) {
+                for (int i = 0; i <= p.size(); i++) {
+                    List<Integer> p_copy = new ArrayList<>(p);
+                    p_copy.add(i, num);
+                    new_perms.add(p_copy);
+                }
+            }
+            perms = new_perms;
+        }
+        return perms;
+    }
+}
